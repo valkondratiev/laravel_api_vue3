8e513c19914d1d96b8a9d4a025c537188e2e0950
valk20 <vkond97@gmail.com>
2023-06-04 15:24:58 +0300

    global auth check - redirect login on fail

--- resources/js/plugins/axios.js
+++ resources/js/plugins/axios.js
@@ -1,14 +1,30 @@
 import axios from 'axios'
 import setAuthHeader from "../../utils/setAuthHeader.js";
+import router from "../router/index.js";
 
 export default {
     install(app) {
         app.config.globalProperties.$axios = axios;
+
+
         if (localStorage.sequreToken) {
             app.config.globalProperties.$axios.defaults.headers = {
                 Authorization: 'Bearer ' + localStorage.sequreToken,
+                Accept:'application/json',
             }
         }
+        app.config.globalProperties.$axios.interceptors.response.use(
+            function(response) {
+                return response;
+            },
+            function(error) {
+                if (error.response.status == 403 || error.response.status == 401) {
+                    localStorage.removeItem('sequreToken');
+                    delete app.config.globalProperties.$axios.defaults.headers.Authorization;
+                    router.push({path: '/login'})
+                }
+            }
+        );
     }
 
 }
