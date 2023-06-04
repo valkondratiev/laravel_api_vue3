99840b57eef8468bb2e63a82a6c3067ddcb3ce85
valk20 <vkond97@gmail.com>
2023-06-04 22:16:26 +0300

    front - date format change

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -21,8 +21,8 @@
                             <option>user</option>
                         </select>
                     </td>
-                    <td>{{ user.created_at }}</td>
-                    <td>
+                    <td>{{ convertDate(user.created_at) }}</td>
+                    <td style="text-align: center;">
                         <button v-if="!user.blocked" @click="user.blocked=1;updateUser('blocked', user)" type="button" class="btn btn-outline-danger">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
                                 <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"></path>
@@ -78,6 +78,9 @@ export default {
                 console.log(error.toJSON());
             })
                 .finally(() => this.loading = false);
+        },
+        convertDate(date){
+            return date.replace('T', ' ').substring(0, date.indexOf('.'));
         }
     },
 }
